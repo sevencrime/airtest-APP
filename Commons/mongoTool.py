98213928cebed9026fb3791523c51cde70297dbe
onedi <sevencrime77@gmail.com>
2019-09-04 23:21:59 +0800

    不好写

--- Commons/mongoTool.py
+++ Commons/mongoTool.py
@@ -9,7 +9,6 @@
 import pymongo
 from bson.objectid import ObjectId
 from collections import Counter
-
 from Commons import Logging
 
 
@@ -44,53 +43,51 @@ class mongoTool:
 
 	def findData(self, database, collection, query):
 
-		db = self.client[database]
-		aosdb = self.client['aos']
-		userdb = self.client['eddidclientpoolfeature']
+		if database == 'test' or database == 'uat':
+			db = self.client[database]
+		elif database == 'aos':
+			aosdb = self.client['aos']
+		elif database == 'eddidclientpoolfeature':
+			userdb = self.client['eddidclientpoolfeature']
+		else:
+			print("错误")
 
-		# 判断先查询哪个数据库的那个表
-		# 记录查询的每个_id, 避免重复
-		# aos.account.idp >> users.subject >> deivers.subjcet >> apply.idp >> apply_info._id >> account.idp >> client_info._id
-		if database == "test" or database == "uat":
-			if collection == "apply_info":
-				findlist = ['apply_info', 'apply', 'account', 'client_info']
+		for result in db[collection].find(query):
 
-		elif database == "aos":
-			pass
+			for key in result.keys():
+				if not key == '_id':
+					if key == 'idpUserId':
+						pass
 
-		elif database == "eddidclientpoolfeature":
-			pass
-
-		for applyinfo in db[collection].find(query):
-			print("applyinfo >> ", applyinfo)
-
-			for apply in db['apply'].find({'_id' : applyinfo['applyId']}):
-				print("apply >> ", apply)
-
-				for account in db['account'].find({'idpUserId' : apply['idpUserId']}):
-					print("account >> ", account)
-
-					if isinstance(account['clientId'], list):
-						for n in range(len(account['clientId'])):
-
-							for client in db['client_info'].find({'_id' : account['clientId'][n]}):
-								print("client_info >> ", client)
-
-					else:
-						print("数据有问题")
-
-				# users 表
-
-				for users in userdb['users'].find({'subject' : apply['idpUserId']}):
-					print("users >> ", users)
-
-				for userdevices in userdb['userdevices'].find({'subject' : apply['idpUserId']}):
-					print("userdevices >> ", userdevices)
-
-
-
-				for aosaccount in aosdb['account'].find({'idpUserId' : apply['idpUserId']}):
-					print("aosaccount >> ", aosaccount)
+		#
+		# for applyinfo in db[collection].find(query):
+		# 	print("applyinfo >> ", applyinfo)
+		#
+		# 	for apply in db['apply'].find({'_id' : applyinfo['applyId']}):
+		# 		print("apply >> ", apply)
+		#
+		# 		for account in db['account'].find({'idpUserId' : apply['idpUserId']}):
+		# 			print("account >> ", account)
+		#
+		# 			if isinstance(account['clientId'], list):
+		# 				for n in range(len(account['clientId'])):
+		#
+		# 					for client in db['client_info'].find({'_id' : account['clientId'][n]}):
+		# 						print("client_info >> ", client)
+		#
+		# 			else:
+		# 				print("数据有问题")
+		#
+		# 		# users 表
+		#
+		# 		for users in userdb['users'].find({'subject' : apply['idpUserId']}):
+		# 			print("users >> ", users)
+		#
+		# 		for userdevices in userdb['userdevices'].find({'subject' : apply['idpUserId']}):
+		# 			print("userdevices >> ", userdevices)
+		#
+		# 		for aosaccount in aosdb['account'].find({'idpUserId' : apply['idpUserId']}):
+		# 			print("aosaccount >> ", aosaccount)
 
 
 	def UpdataData(self):
@@ -101,14 +98,14 @@ class mongoTool:
 
 
 if __name__ == '__main__':
-	host = 'mongodb+srv://eddiddevadmin:atfxdev2018@dev-clientdb-nckz7.mongodb.net'
-	# host = 'localhost:27017'
-	database = 'test'	#查询的数据库
+	# host = 'mongodb+srv://eddiddevadmin:atfxdev2018@dev-clientdb-nckz7.mongodb.net'
+	host = 'localhost:27017'
+	database = 'uat'	#查询的数据库
 	# Database(host, database).del_linked("apply_debug", {"phone":"13709691525"})	# 传入需要查询的表和查询条件
 	# Database(host, database).del_linked("apply_debug", {'email':{"$regex" : ".*onedi.*"}})
 
 	d = mongoTool(host)
-	d.findData(database, "apply_info", {'email':'901onedi2s1067@qq.com'})
+	d.findData(database, "apply_info", {'phone':'6880062539507'})
 	
 
 
