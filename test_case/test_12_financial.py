a4494b02597aa6df68b9007e3999af39b5e2113f
onedi <sevencrime77@gmail.com>
2019-10-08 22:26:58 +0800

    本地提交

--- test_case/test_12_financial.py
+++ test_case/test_12_financial.py
@@ -2,16 +2,20 @@
 # -*- coding: utf-8 -*-
 import allure
 import pytest
-
+from airtest.core.api import *
 from ElementPage.financialPage import financialPage
 from ElementPage.publicTool import publicTool
 
 @allure.feature("相关保证金融资账户")
 class Test_financial():
 
-    @allure.story("相关保证金融资账户")
-    @pytest.mark.run(order=3)
-    def test_derivative(self, poco):
+    fix_routetitle = ["相关保证金融资账户"]
+
+    @allure.story("没有勾选(证券保证金), 正常输入")
+    @pytest.mark.parametrize("accountHolder", [True, False])
+    @pytest.mark.parametrize("orderPerson", [True, False])
+    @pytest.mark.parametrize("reloadRoute", fix_routetitle, indirect=True)
+    def test_derivative(self, poco, reloadRoute, orderPerson, accountHolder):
         pubTool = publicTool(poco)
         financial = financialPage(poco)
         with allure.step("您是否是账户的最终实益拥有人? "):
@@ -22,6 +26,31 @@ class Test_financial():
 
         with allure.step("点击下一步"):
             pubTool.click_NextStepbtn()
+            assert_equal(pubTool.get_Routetitle(), "其他资料", msg="页面跳转到{}页面".format(pubTool.get_Routetitle()))
+
+
+
+    @allure.story("勾选(证券保证金), 正常输入")
+    @pytest.mark.parametrize("accountHolder", [True, False])
+    @pytest.mark.parametrize("orderPerson", [True, False])
+    @pytest.mark.parametrize("reloadRoute", fix_routetitle, indirect=True)
+    @pytest.mark.skipif()
+    def test_derivative(self, poco, reloadRoute, orderPerson, accountHolder):
+        pubTool = publicTool(poco)
+        financial = financialPage(poco)
+        with allure.step("您是否是账户的最终实益拥有人? "):
+            financial.click_accountHolder(True)
+
+        with allure.step("您是否是最终负责下单的人?? "):
+            financial.click_orderPerson(True)
+
+        with allure.step("点击下一步"):
+            pubTool.click_NextStepbtn()
+            assert_equal(pubTool.get_Routetitle(), "其他资料", msg="页面跳转到{}页面".format(pubTool.get_Routetitle()))
+
+
+
+
 
 
 if __name__ == "__main__":
